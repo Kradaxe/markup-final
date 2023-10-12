c492257579df5c9edaa89968f0c10edac6c2bd9d
Harshit Rawat <rawatharshit300@gmail.com>
2023-10-12 21:25:28 +0530

    migrated commit

--- youtube2022-react-admin/src/components/workspace/Workspace.jsx
+++ youtube2022-react-admin/src/components/workspace/Workspace.jsx
@@ -12,6 +12,8 @@ export const Workspace = () => {
   const [finalimag, setfinalimag] = useState("");
   const { currentImg, setCurrentImg } = useContext(MyContext);
   const navigator = useNavigate();
+  const [shareableToken, setshareableToken] = useState("");
+  console.log(shareableToken);
   const handleFileChange = (e) => {
     setSelectedFile(e.target.files[0]);
   };
@@ -27,7 +29,7 @@ export const Workspace = () => {
         axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
         const response = await axios
-          .post("http://localhost:8000/api/upload", formData, {
+          .post("http://localhost:3000/api/images/post", formData, {
             headers: {
               "Content-Type": "multipart/form-data",
             },
@@ -82,11 +84,30 @@ export const Workspace = () => {
           "Authorization"
         ] = `Bearer ${response.data.token}`;
         console.log(response);
+        // const shareToken = response.data.body.shareToken;
+        // console.log(shareToken);
         setAllImage(response.data.body);
       });
   }
   useEffect(() => {
     getImage();
+  }, []);
+  async function getImagebytoken() {
+    await axios
+      .get("http://localhost:8000/api/posts/ZoSbj1MoPT")
+      .then((response) => {
+        console.log(response.data);
+        localStorage.getItem("token", response.data.token);
+        // axios.defaults.headers.common[
+        //   "Authorization"
+        // ] = `Bearer ${response.data.token}`;
+        console.log(response.data.body.shareToken);
+        setshareableToken(response.data.body.shareToken);
+        // setAllImage(response.data.body);
+      });
+  }
+  useEffect(() => {
+    getImagebytoken();
   }, []);
 
   return (
@@ -94,7 +115,6 @@ export const Workspace = () => {
       <div className="searchtab">
         <Searchtab />
       </div>
-
       <a href="/ImageComments">COMMENTS</a>
 
       <div className="mainsection" style={{ width: "auto" }}>
@@ -117,6 +137,7 @@ export const Workspace = () => {
             <img onClick={send} width={100} height={100} src={data.photo} />
           );
         })}
+        {/* <img onClick={getImagebytoken} width={100} height={100} src="" /> */}
       </div>
     </div>
   );
